b1a41bba14ae5d27f0669fa697e3035e60a569c1
Myron <dubtwee@gmail.com>
2019-04-19 10:15:58 -0500

    creating a way to remove red boxes with the box itself is clicked on

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
     $('#blueBtn').on('click', onClickForBlue);
     $('#greenBtn').on('click', onClickForGreen);
     $('#yellowBtn').on('click', onClickForYellow);
+
+    $('#container').on('click', onClickContainerRemoveBox);
 });
 
 const counter = {
@@ -28,6 +30,15 @@ function onClickForYellow() {
     displayBox('yellow');
 }
 
+function onClickContainerRemoveBox(event) {
+    const $clickedChild = $(event.target);
+    const elemClasses = $clickedChild.attr('class');
+
+    if (elemClasses.indexOf('red') != -1) {
+        removeDisplayBox('red', $clickedChild);
+    }
+}
+
 function displayBox(color) {
     const firstLetter = color.charAt(0);
     const uppercaseFirstLetter = firstLetter.toUpperCase();
@@ -38,6 +49,17 @@ function displayBox(color) {
 
     $(`#${color}Count`).text(countText);
     $('#container').append(`<div class="box ${color}"></div>`);
+}
+
+function removeDisplayBox(color, $boxElem) {
+    const firstLetter = color.charAt(0);
+    const uppercaseFirstLetter = firstLetter.toUpperCase();
+    const countLabel = uppercaseFirstLetter + color.slice(1);
+
+    counter[color]--;
+    const countText = `${countLabel} Count: ${counter[color]}`;
+    $(`#${color}Count`).text(countText);
+    $boxElem.remove();
 }
 
 console.log('Im running outside the document ready!');
